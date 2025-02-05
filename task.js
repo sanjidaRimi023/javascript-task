d15426cb722a2d44650ab1ab8a82f2489bf8d2d8
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-02-05 19:56:04 +0600

    javascript task result

---
+++ task.js
@@ -0,0 +1,20 @@
+// 1st task
+
+var money = 3000;
+var applePrice = 100;
+var orangePrice = 2000;
+var totalPrice = applePrice + orangePrice;
+change = money - totalPrice;
+console.log(change);
+
+
+// 2nd task
+
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var Bangle = 99.50;
+var totalMark =(math + biology + chemistry + physics + Bangle);
+average = totalMark/5;
+console.log(average);
