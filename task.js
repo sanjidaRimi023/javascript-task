a5dae935f09164ac1421e369db7b2a5c3924c710
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-02-08 15:59:23 +0600

    added some javascript task and solution

--- task.js
+++ task.js
@@ -15,6 +15,91 @@ var biology = 65;
 var chemistry = 80;
 var physics = 35.45;
 var Bangle = 99.50;
-var totalMark =(math + biology + chemistry + physics + Bangle);
+var totalMark =( math + biology + chemistry + physics + Bangle );
 average = totalMark/5;
 console.log(average);
+
+
+// burger task
+
+var burgerPrice = 200;
+
+if (burgerPrice > 500) {
+    console.log("get a free coke with love");
+}
+else {
+    console.log("coke 30 tk");
+}
+
+/*** 
+BMI Calculator and Health Category
+Create a JavaScript program that calculates the Body Mass Index (BMI) and assigns a health category based on the BMI value. Use nested if-else statements to determine the health category
+    - Calculate BMI using the formula: BMI = weight (kg) / (height (m))^2
+    - BMI < 18.5, you are underweight.
+    - BMI >= 18.5 and BMI <=24.9, you are normal.
+    - BMI >=25 and BMI <= 29.9, you are overweight.
+    - Otherwise, you are obese.
+*/
+var BMI = 21.5;
+if ( BMI < 18.5) {
+    console.log("you are underweight.");
+} else if ( BMI >= 18.5 && BMI <= 24.9) {
+    console.log("you are normal.")
+} else if ( BMI >= 25 && BMI <= 29.9) {
+    console.log("you are overweight!")
+} else {
+    console.log("you are obese.")
+}
+
+// 4th task about grade calculator
+/***
+Grade Calculator:
+Create a simple JavaScript program that takes a student's score as input and returns their corresponding grade based on the following grading scale:
+    A: 90-100, B: 80-89,  C: 70-79, D: 60-69, F: 0-59
+***/
+let mark = 71;
+if (mark >= 90) {
+    console.log("your grade : A");
+}
+else if (mark >= 80) {
+    console.log("your grade : B");
+}
+else if ( mark >= 70) {
+    console.log("your grade : C");
+}
+else if ( mark >= 60) {
+    console.log("your grade : D");
+}
+else {
+    console.log("you are fall!!")
+}
+
+/***
+if you get more then 80 then inside your friend score. 
+    If your friend get more than 80. then go for a lunch. 
+    if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+    if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+    if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+*/
+
+let myResult = 99;
+let myFriendResult = 79;
+
+if (myResult >80){
+    if(myFriendResult > 80) {
+        console.log("let's go a lunch");
+    }
+    else if (myFriendResult >= 60) {
+        console.log("good luck for next time");
+    }
+    else if (myFriendResult >= 40) {
+        console.log("message unseen..!!");
+    }
+    else{
+        console.log("go to hell..!!");
+    }
+}
+else {
+    console.log("go to home and sleep and act very sad i'm :( ");
+}
